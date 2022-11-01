56f74ecab1830a74685aae11fe508486fb44c4e1
ArtiemX <116341560+ArtiemX@users.noreply.github.com>
2022-11-01 22:41:39 +0900

    Update .h

--- 2/.h
+++ 2/.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <iostream>
 
+using namespace std;
+
+class //Enter your code { 
+private:
+//Enter your code
+public:
+//Enter your code
+};
+
+class //Enter your code {
+private:
+//Enter your code
+public:
+//Enter your code
+};
