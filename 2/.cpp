d42587882b491336468929d81c644f2d440f2d05
ArtiemX <116341560+ArtiemX@users.noreply.github.com>
2022-11-01 22:39:41 +0900

    Update .cpp

--- 2/.cpp
+++ 2/.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+//Enter your code
+using namespace std;
 
+//Enter your code
+
+void fight(){
+//Enter your code
+}
+
+void Showfight(){
+  cout << "플레이어의 체력은:" << //Enter your code(해당 객체의 체력) << endl;
+	cout << "몬스터의 체력은:" << //Enter your code(해당 객체의 체력) << endl;
+}
+
+int main() {
+
+	fight(); //첫 번째 싸움
+	fight(); //두 번째 싸움
+	fight(); //세 번째 싸움
+
+	Showfight(); // 싸움 결과.
+}
