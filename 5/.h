41b652156336d39bd4912c733cbefe7c8344de0f
Kim han jun <116341560+Artiespirre@users.noreply.github.com>
2022-11-15 21:52:43 +0900

    Create .h

---
+++ 5/.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <random>
+
+std::random_device //Enter your code
+std::mt19937 //Enter your code	
+std::uniform_int_distribution<int> //Enter your code
+
+//Enter your code
