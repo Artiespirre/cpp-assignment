ed64811d6bb7961208af0b17dca0c774a082d791
Kim han jun <116341560+Artiespirre@users.noreply.github.com>
2022-12-06 22:15:09 +0900

    Create .h

---
+++ 10/.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Circle{
+//Enter your code
+  
+public:
+  void showArea(){
+  cout<<"원의 넓이:"<< //Enter your code <<endl;
+  }
+};
